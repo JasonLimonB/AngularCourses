ad6975a1994393a12507924dc4af734735d0ef15
Jason <jason.limon@outlook.com>
2021-05-28 11:30:46 -0500

    Drag and Drop

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { CommonModule } from '@angular/common';
 import { DescriptionComponent } from './home/description/description.component';
@@ -7,21 +8,25 @@ import { MypipePipe } from '../pipes/mypipe.pipe';
 
 
 import { MyvirtualscrollComponent } from './myvirtualscroll/myvirtualscroll.component';
+import { DraganddropComponent } from './draganddrop/draganddrop.component';
 
 
 @NgModule({
   declarations: [
     DescriptionComponent,
     MypipePipe,
-    MyvirtualscrollComponent
+    MyvirtualscrollComponent,
+    DraganddropComponent
   ],
   imports: [
     CommonModule,
-    ScrollingModule
+    ScrollingModule,
+    DragDropModule
   ],
   exports: [
     DescriptionComponent,
-    MyvirtualscrollComponent
+    MyvirtualscrollComponent,
+    DraganddropComponent
   ]
 })
 export class ComponentsModule { }
